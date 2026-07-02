1f7bcaa0d16e2ee12b7341152a4b3531fd09b809
WhyNotDogie <112395091+WhyNotDogie@users.noreply.github.com>
2026-07-02 11:05:23 +0200

    fix: remove fake impaling from creative tab

--- src/main/java/dev/dogie/tactical_tridents/client/TTCreativeTabEvents.java
+++ src/main/java/dev/dogie/tactical_tridents/client/TTCreativeTabEvents.java
@@ -4,6 +4,7 @@ import dev.dogie.tactical_tridents.TacticalTridents;
 import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.world.item.CreativeModeTabs;
 import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.Items;
 import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.fml.common.EventBusSubscriber;
 import net.neoforged.neoforge.event.BuildCreativeModeTabContentsEvent;
@@ -19,5 +20,24 @@ public class TTCreativeTabEvents {
                     CreativeModeTab.TabVisibility.PARENT_AND_SEARCH_TABS
             );
         }
+        if (event.getTabKey() == CreativeModeTabs.INGREDIENTS || event.getTabKey() == CreativeModeTabs.OP_BLOCKS) {
+            event.getParentEntries().removeIf(TTCreativeTabEvents::isHiddenBook);
+            event.getSearchEntries().removeIf(TTCreativeTabEvents::isHiddenBook);
+        }
+    }
+    private static boolean isHiddenBook(ItemStack stack) {
+        if (stack.is(Items.ENCHANTED_BOOK)) {
+            var enchantments = stack.getTagEnchantments().keySet();
+            for (var enchantmentHolder : enchantments) {
+                if (enchantmentHolder.unwrapKey().isPresent()) {
+                    var key = enchantmentHolder.unwrapKey().get();
+                    if (key.location().getNamespace().equals("tactical_tridents") &&
+                            key.location().getPath().equals("fake_impaling")) {
+                        return true;
+                    }
+                }
+            }
+        }
+        return false;
     }
 }
