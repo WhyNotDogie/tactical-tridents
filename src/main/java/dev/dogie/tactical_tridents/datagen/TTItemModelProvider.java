3885696888bae0a17879fa7041c1a0f5b9e77ca4
PouffyDev <99536749+pouffy@users.noreply.github.com>
2026-07-11 02:07:35 +0100

    Better trident rendering

--- src/main/java/dev/dogie/tactical_tridents/datagen/TTItemModelProvider.java
+++ src/main/java/dev/dogie/tactical_tridents/datagen/TTItemModelProvider.java
@@ -1,14 +1,9 @@
 package dev.dogie.tactical_tridents.datagen;
 
 import dev.dogie.tactical_tridents.TacticalTridents;
-import net.minecraft.client.renderer.block.model.BlockModel;
 import net.minecraft.data.PackOutput;
 import net.minecraft.resources.ResourceLocation;
-import net.minecraft.world.item.ItemDisplayContext;
-import net.neoforged.neoforge.client.model.generators.ItemModelBuilder;
 import net.neoforged.neoforge.client.model.generators.ItemModelProvider;
-import net.neoforged.neoforge.client.model.generators.ModelFile;
-import net.neoforged.neoforge.client.model.generators.loaders.SeparateTransformsModelBuilder;
 import net.neoforged.neoforge.common.data.ExistingFileHelper;
 
 public class TTItemModelProvider extends ItemModelProvider {
@@ -19,39 +14,6 @@ public class TTItemModelProvider extends ItemModelProvider {
 
     @Override
     protected void registerModels() {
-        ModelFile itemGenerated = new ModelFile.UncheckedModelFile("item/generated");
-        ModelFile builtinEntity = new ModelFile.UncheckedModelFile("builtin/entity");
-
-        ItemModelBuilder tridentInventory2D = ((ItemModelBuilder) nested()).parent(itemGenerated)
-                .texture("layer0", ResourceLocation.fromNamespaceAndPath("tactical_tridents", "item/netherite_trident"));
-
-        ItemModelBuilder tridentHand3D = ((ItemModelBuilder) nested()).parent(builtinEntity)
-                .guiLight(BlockModel.GuiLight.FRONT)
-                .texture("particle", ResourceLocation.fromNamespaceAndPath("tactical_tridents", "item/netherite_trident"));
-
-        tridentHand3D.transforms()
-                .transform(ItemDisplayContext.THIRD_PERSON_RIGHT_HAND)
-                .rotation(0F, 60F, 0F).translation(11F, 17F, -2F).scale(1.0F, 1.0F, 1.0F).end()
-                .transform(ItemDisplayContext.THIRD_PERSON_LEFT_HAND)
-                .rotation(0F, 60F, 0F).translation(3F, 17F, 12F).scale(1.0F, 1.0F, 1.0F).end()
-                .transform(ItemDisplayContext.FIRST_PERSON_RIGHT_HAND)
-                .rotation(0F, -90F, 25F).translation(-3F, 17F, 1F).scale(1.0F, 1.0F, 1.0F).end()
-                .transform(ItemDisplayContext.FIRST_PERSON_LEFT_HAND)
-                .rotation(0F, 90F, -25F).translation(13F, 17F, 1F).scale(1.0F, 1.0F, 1.0F).end()
-                .transform(ItemDisplayContext.GUI)
-                .rotation(15F, -25F, -5F).translation(2F, 3F, 0F).scale(0.65F, 0.65F, 0.65F).end()
-                .transform(ItemDisplayContext.FIXED)
-                .rotation(0F, 180F, 0F).translation(-2F, 4F, -5F).scale(0.5F, 0.5F, 0.5F).end()
-                .transform(ItemDisplayContext.GROUND)
-                .rotation(0F, 0F, 0F).translation(4F, 4F, 2F).scale(0.25F, 0.25F, 0.25F).end();
-
-        withExistingParent("netherite_trident", "item/generated")
-                .customLoader(SeparateTransformsModelBuilder::begin)
-                .base(tridentInventory2D)
-                .perspective(ItemDisplayContext.FIRST_PERSON_RIGHT_HAND, tridentHand3D) // 3D offsets for player perspectives
-                .perspective(ItemDisplayContext.FIRST_PERSON_LEFT_HAND, tridentHand3D)
-                .perspective(ItemDisplayContext.THIRD_PERSON_RIGHT_HAND, tridentHand3D)
-                .perspective(ItemDisplayContext.THIRD_PERSON_LEFT_HAND, tridentHand3D)
-                .perspective(ItemDisplayContext.GROUND, tridentHand3D);
+        withExistingParent("netherite_trident", "item/generated").texture("layer0", ResourceLocation.fromNamespaceAndPath("tactical_tridents", "item/netherite_trident"));
     }
 }
--- src/main/java/dev/dogie/tactical_tridents/netherite_trident/render/NetheriteTridentModel.java
+++ src/main/java/dev/dogie/tactical_tridents/netherite_trident/render/NetheriteTridentModel.java
@@ -5,40 +5,49 @@ package dev.dogie.tactical_tridents.netherite_trident.render;// Made with Blockb
 
 import com.mojang.blaze3d.vertex.PoseStack;
 import com.mojang.blaze3d.vertex.VertexConsumer;
+import dev.dogie.tactical_tridents.TacticalTridents;
 import net.minecraft.client.model.Model;
 import net.minecraft.client.model.geom.ModelPart;
 import net.minecraft.client.model.geom.PartPose;
 import net.minecraft.client.model.geom.builders.*;
 import net.minecraft.client.renderer.RenderType;
+import net.minecraft.resources.ResourceLocation;
 
 // i am never changing the model again cus wtf did i even do to make this work
 
+// I changed this model :3 - Krystal
+
 public class NetheriteTridentModel extends Model {
-	private final ModelPart root;
+	public static final ResourceLocation TEXTURE = ResourceLocation.fromNamespaceAndPath(TacticalTridents.MODID, "textures/entity/netherite_trident.png");
+
+	private final ModelPart trident;
+	private final ModelPart body;
 
 	public NetheriteTridentModel(ModelPart root) {
 		super(RenderType::entityCutout);
-		this.root = root;
+		this.trident = root.getChild("trident");
+		this.body = this.trident.getChild("body");
 	}
 
 	public static LayerDefinition createBodyLayer() {
 		MeshDefinition meshdefinition = new MeshDefinition();
 		PartDefinition partdefinition = meshdefinition.getRoot();
 
-		PartDefinition bb_main = partdefinition.addOrReplaceChild("bb_main", CubeListBuilder.create().texOffs(0, 0).addBox(-0.5F, -30.0F, -0.5F, 1.0F, 30.0F, 1.0F, new CubeDeformation(0.0F))
-				.texOffs(4, 26).addBox(-3.5F, -26.0F, 0.0F, 7.0F, 5.0F, 0.0F, new CubeDeformation(0.0F))
-				.texOffs(4, 23).addBox(-1.5F, -26.0F, -0.5F, 1.0F, 2.0F, 1.0F, new CubeDeformation(0.0F))
-				.texOffs(4, 23).addBox(0.5F, -26.0F, -0.5F, 1.0F, 2.0F, 1.0F, new CubeDeformation(0.0F))
-				.texOffs(8, 24).addBox(1.5F, -26.0F, -0.5F, 1.0F, 1.0F, 1.0F, new CubeDeformation(0.0F))
-				.texOffs(4, 16).addBox(-2.5F, -26.0F, -0.5F, 1.0F, 1.0F, 1.0F, new CubeDeformation(0.0F))
-				.texOffs(4, 18).addBox(-3.5F, -29.0F, -0.5F, 1.0F, 4.0F, 1.0F, new CubeDeformation(0.0F))
-				.texOffs(4, 18).addBox(2.5F, -29.0F, -0.5F, 1.0F, 4.0F, 1.0F, new CubeDeformation(0.0F)), PartPose.offset(0.0F, 24.0F, 0.0F));
+		PartDefinition trident = partdefinition.addOrReplaceChild("trident", CubeListBuilder.create().texOffs(0, 0).addBox(-0.5F, -15.0F, -0.5F, 1.0F, 30.0F, 1.0F, new CubeDeformation(0.0F))
+				.texOffs(4, 18).addBox(2.5F, -14.0F, -0.5F, 1.0F, 4.0F, 1.0F, new CubeDeformation(0.0F))
+				.texOffs(4, 18).addBox(-3.5F, -14.0F, -0.5F, 1.0F, 4.0F, 1.0F, new CubeDeformation(0.0F)), PartPose.offset(0.0F, 9.0F, 0.0F));
+
+		PartDefinition body = trident.addOrReplaceChild("body", CubeListBuilder.create().texOffs(4, 23).addBox(0.5F, -11.0F, -0.5F, 1.0F, 2.0F, 1.0F, new CubeDeformation(0.0F))
+				.texOffs(4, 23).addBox(-1.5F, -11.0F, -0.5F, 1.0F, 2.0F, 1.0F, new CubeDeformation(0.0F))
+				.texOffs(4, 16).addBox(-2.5F, -11.0F, -0.5F, 1.0F, 1.0F, 1.0F, new CubeDeformation(0.0F))
+				.texOffs(4, 16).addBox(1.5F, -11.0F, -0.5F, 1.0F, 1.0F, 1.0F, new CubeDeformation(0.0F))
+				.texOffs(4, 26).addBox(-3.5F, -11.0F, 0.0F, 7.0F, 5.0F, 0.0F, new CubeDeformation(0.0F)), PartPose.offset(0.0F, 0.0F, 0.0F));
 
 		return LayerDefinition.create(meshdefinition, 32, 32);
 	}
 
 	@Override
 	public void renderToBuffer(PoseStack poseStack, VertexConsumer vertexConsumer, int i1, int i2, int i3) {
-		root.render(poseStack, vertexConsumer, i1, i2, i3);
+		trident.render(poseStack, vertexConsumer, i1, i2, i3);
 	}
 }
